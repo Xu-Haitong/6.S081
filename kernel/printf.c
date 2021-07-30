be715646c756a3c6d8b854ae34fa7377aa9b91c1
xuhaitong-VMware <2452009536@qq.com>
2021-07-30 03:55:34 -0700

    "Backtrace" done

--- kernel/printf.c
+++ kernel/printf.c
@@ -117,6 +117,7 @@ printf(char *fmt, ...)
 void
 panic(char *s)
 {
+  backtrace();
   pr.locking = 0;
   printf("panic: ");
   printf(s);
@@ -131,4 +132,22 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+// 打印栈帧
+void 
+backtrace(void)
+{
+  uint64 fp = r_fp();
+  // 找到栈底结束
+  uint64 bottom = PGROUNDUP(fp);
+  uint64 return_addr;
+  
+  printf("backtrace:\n");
+  while (fp < bottom)
+  {
+    return_addr = *(uint64*)(fp-8);
+    fp = *(uint64*)(fp-16);
+    printf("%p\n", return_addr);
+  }
 }
